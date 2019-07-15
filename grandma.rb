ac2a7f198000880855aa30de497036404e037cb4
Randall Taylor <randyisbusy@gmail.com>
2019-07-15 00:16:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,11 +5,12 @@ def speak_to_grandma(talk)
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH? SPEAK UP, SONNY!"
+  end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
